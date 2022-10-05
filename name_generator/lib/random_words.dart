9bdcd79612171b410627d28605974a20c007fd16
Mujore Umme Faatimah-Iz-Zaahra <74302462+fz3hra@users.noreply.github.com>
2022-10-05 20:32:30 +0400

    help

--- name_generator/lib/random_words.dart
+++ name_generator/lib/random_words.dart
@@ -75,7 +75,22 @@ class RandomWordsState extends State<RandomWords> {
               icon: Icon(Icons.list),
               onPressed: () {
                 Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => Another()));
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  final Iterable<ListTile> tiles =
+                      _savedWordpairs.map((WordPair pair) {
+                    return ListTile(
+                      title: Text(pair.asPascalCase,
+                          style: TextStyle(fontSize: 16.0)),
+                    );
+                  });
+                  final List<Widget> divided =
+                      ListTile.divideTiles(context: context, tiles: tiles)
+                          .toList();
+
+                  return Scaffold(
+                      appBar: AppBar(title: Text('Saved Wordpairs')),
+                      body: ListView(children: divided));
+                }));
               })
         ],
       ),
